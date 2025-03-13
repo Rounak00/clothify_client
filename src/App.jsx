f407443b0cd66bf8cca846b5535d56deb408e826
Rounak-Mukherjee-04 <web.dev.mi06@gmail.com>
2025-03-13 22:08:33 +0530

    added the home componenets

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,11 @@ import React from 'react';
 import {BrowserRouter,Route, Routes} from "react-router-dom"
 import UserLayout from './components/Layout/UserLayout';
 import Home from './pages/Home';
-
+import {Toaster} from "react-hot-toast"
 const App = () => {
   return (
     <BrowserRouter>
+      <Toaster position='top-right' gutter={8} />
     <Routes>
       <Route path="/" element={<UserLayout/>}>
         <Route index element={<Home/>}/>
--- src/components/products/NewArrivals.jsx
+++ src/components/products/NewArrivals.jsx
@@ -6,7 +6,7 @@ const NewArrivals = () => {
     const scrollRef = useRef(null);
     const [isDragging, setIsDragging] = useState(false);
     const [startX, setStartX] = useState(0);
-    const [scrollLeft, setScrollRight] = useState(false);
+    const [scrollLeft, setScrollLeft] = useState(false);
     const [canScrollLeft, setCanScrollLeft] = useState(false);
     const [canScrollRight, setCanScrollRight] = useState(true);
     const newArrivals = [
@@ -88,6 +88,22 @@ const NewArrivals = () => {
             ],
         },
     ];
+    const handleMouseDown=(e)=>{
+        setIsDragging(true);
+        setStartX(e.pageX -scrollRef.current.offsetLeft);
+        setScrollLeft(scrollRef.current.scrollLeft);
+    }
+
+    const handleMouseMove=(e)=>{
+        if(!isDragging) return;
+        const x=e.pageX - scrollRef.current.offsetLeft;
+        const walk=x-startX;
+        scrollRef.current.scrollLeft=scrollLeft-walk;
+    }
+
+    const handleMouseUpOrLeave=()=>{
+        setIsDragging(false);
+    }
 
     const scroll = (direction) => {
         const scrollAmount = direction === "left" ? -300 : 300;
@@ -107,10 +123,12 @@ const NewArrivals = () => {
         const container = scrollRef.current;
         if (container) {
             container.addEventListener("scroll", updateScrollButtons);
+            updateScrollButtons();
+            return ()=> container.removeEventListener("scroll",updateScrollButtons)
         }
-    }, [])
+    })
     return (
-        <section>
+        <section className='py-16 px-4 lg:px-0'>
             <div className='container mx-auto text-center mb-10 relative'>
                 <h2 className='text-3xl font-bold mb-4'>Explore New Arrivals</h2>
                 <p className='text-lg text-gray-600 mb-8'>
@@ -128,11 +146,16 @@ const NewArrivals = () => {
                     </button>
                 </div>
             </div>
-            <div ref={scrollRef} className='container mx-auto overflow-x-scroll flex space-x-6 relative'>
+            <div ref={scrollRef} 
+            onMouseDown={handleMouseDown}
+            onMouseMove={handleMouseMove}
+            onMouseUp={handleMouseUpOrLeave}
+            onMouseLeave={handleMouseUpOrLeave}
+            className={`container mx-auto overflow-x-scroll flex space-x-6 relative ${isDragging ? "cursor-grabbing":"cursor-grab"}`}>
                 {newArrivals.map((product) => (
                     <div key={product._id} className='min-w-[100
             %] sm:min-w-[50%] lg:min-w-[30%] relative'>
-                        <img src={product.images[0]?.url} alt={product.images[0]?.altText} className='w-full h-[500px] object-cover rounded-lg' />
+                        <img src={product.images[0]?.url} alt={product.images[0]?.altText} draggable="false" className='w-full h-[500px] object-cover rounded-lg' />
                         <div className='absolute bottom-0 left-0 right-0 opacity-80 backdrop-blur-md p-4 text-white rounded-b-lg'>
                             <Link to={`/product/${product._id}`} className='block'>
                                 <h4 className='font-medium'>{product.name}</h4>
