50546ae239daf07f2d7b27cfac044e664b6ca4af
Rounak00 <mukherjeerounak10@gmail.com>
2025-04-09 22:07:03 +0530

    Added

--- src/components/products/FilterSidebar.jsx
+++ src/components/products/FilterSidebar.jsx
@@ -54,7 +54,7 @@ const FilterSidebar = () => {
     }
 
     const categories = ["Top Wear", "Bottom Wear"];
-    const colors = ["Red", "Blue", "Black", "Green", "Yellow", "Gray", "White", "Pink", "Beige", "Navy"];
+    const colors = ["Red", "Blue", "Black", "Green", "Yellow", "Gray", "White", "Pink"];
     const sizes = ["XS", "S", "M", "L", "XL", "XXL"];
     const materials = ["Cotton", "Wool", "Denim", "Polyester", "Silk", "Linen", "Viscose", "Fleece"];
     const brands = ["Urban Threads", "Modern Fit", "Street Style", "Beach Breeze", "Fashionista", "ChicStyle"];
